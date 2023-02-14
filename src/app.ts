9d4200ee8f8cbf54911992cc3369b4afb57655be
bedirhan <celayirbedirhan@gmail.com>
2023-02-14 18:25:36 +0300

    feat: attempt to collect multistep form data

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { Context, Markup, Telegraf } from "telegraf";
 import { Update } from "typegram";
 import { welcomeText } from "./constants";
 import axios from "axios";
-import { downloadFile } from "./utils";
+import { downloadFile, saveFormDataForSubmission } from "./utils";
 
 const bot: Telegraf<Context<Update>> = new Telegraf(
 	(process.env.BOT_TOKEN as string) ||
@@ -13,29 +13,37 @@ bot.start((ctx) => ctx.reply(welcomeText));
 
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 
-bot.command("sharenote", (ctx) => {
-	ctx.reply(
-		"Not tipi",
-		Markup.inlineKeyboard([
+bot.command("sharenote", async (ctx) => {
+	const x = await ctx.reply(
+		"Paylaşmak istediğiniz notun başlığı ne olsun",
+		/* Markup.inlineKeyboard([
 			Markup.button.callback("Fotoğraf", "first"),
 			Markup.button.callback("PDF", "second"),
-		])
+		]) */
+		/* Markup.keyboard(["Fotoğraf", "PDF"]).oneTime().resize() */
+		Markup.forceReply()
 	);
 });
-bot.command("PDF", (ctx) => {
+
+bot.action("first", (ctx) => {
 	ctx.reply(
 		"Not tipi",
 		Markup.inlineKeyboard([
-			Markup.button.callback("Yükle", "first"),
-			Markup.button.callback("Boşver", "second"),
+			Markup.button.callback("X", "first"),
+			Markup.button.callback("Y", "second"),
 		])
 	);
 });
+
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 
 bot.on("document", async (ctx) => {
+	const userName = ctx.update.message.from.username;
+	const chatId = ctx.update.message.chat.id;
+	console.log(userName, chatId);
 	const { file_id: fileId, file_name } = ctx.update.message.document;
 	const fileUrl = await ctx.telegram.getFileLink(fileId);
+
 	/* TODO: send url or file itself to place for saving */
 	/* const response = await axios.get(fileUrl.toString()); */
 	await downloadFile(fileUrl.toString(), "./cache/" + file_name);
@@ -46,8 +54,11 @@ bot.on("document", async (ctx) => {
 });
 
 bot.on("message", (ctx) => {
-	console.log(ctx);
-	ctx.reply("helloooo");
+	const userID = ctx.update.message.from.id;
+	const mess = ctx.update.message as any;
+	const message = mess?.text;
+	const currentStatus = saveFormDataForSubmission({ userID, message });
+	ctx.reply("Please enter " + currentStatus);
 });
 
 bot.launch().then(() => console.log("Bot started"));
--- src/utils.ts
+++ src/utils.ts
@@ -28,3 +28,34 @@ export async function downloadFile(
 		});
 	});
 }
+
+export const formState: any = {};
+const formKeys = ["title", "description"];
+
+export const saveFormDataForSubmission = (data: {
+	userID: number;
+	message: string;
+}) => {
+	const { userID } = data;
+	if (!(userID in formState)) {
+		formState[userID] = {};
+	}
+	const keyCurr = checkCurrentStateOfUser(userID);
+
+	if (userID in formState) {
+		formState[userID][keyCurr] = data.message;
+	}
+	return keyCurr;
+};
+const checkCurrentStateOfUser = (userID: number) => {
+	// formKeys'den hangisini tamamlamış en son
+	for (let index = 0; index < formKeys.length; index++) {
+		const key = formKeys[index];
+		if (key in formState[userID]) {
+		} else {
+			// this is the key that user should fill
+			return key;
+		}
+	}
+	return "finished";
+};
